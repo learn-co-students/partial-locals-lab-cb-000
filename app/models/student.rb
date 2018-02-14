06537be7577635806584b58acd342b6d96cb430c
Heather Christman <efl7a@yahoo.de>
2018-02-14 10:00:08 -0500

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,20 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(search)
+    if search
+      # name = name.downcase
+      # students_with_name = []
+      # self.all.each do |student|
+      #   if student.name.downcase.include?(name)
+      #     students_with_name << student
+      #   end
+      # end
+      # students_with_name
+      self.where('name LIKE ?', "%#{search}%").all
+    else
+      self.all
+    end
+  end
 end
