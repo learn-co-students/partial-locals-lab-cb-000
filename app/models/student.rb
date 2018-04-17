ae5c4a2a2b6783d2aff868c495ed9aa31833306a
adam n <adam.nawroc@gmail.com>
2018-04-17 13:33:34 +0000

    progress

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,15 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(term)
+
+    if term.empty?
+      Student.all
+    else
+      Student.where('name LIKE ?', "%" + term + "%").all
+
+    end
+
+  end
 end
